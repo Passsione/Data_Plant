9cc94d3ff8747cf46eadcc3d43f5be1c272e5758
Mogale Tshehla <102671206+Passsione@users.noreply.github.com>
2025-04-13 15:19:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,8 @@ def create_app():
         )
     
     return app
-
+app = create_app()
 if __name__ == '__main__':
-    app = create_app()
     with app.app_context():
         from extensions import db
         db.create_all()
